80e2bae81e8c09dd3d1c2890e73fd2ce85f7399d
bryan hunt <github@binarytemple.co.uk>
2016-01-05 11:25:33 +0000

    improved config, extended example. TODO: class reloading.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # poc_agent
 
 A simple example of implementing a Java agent. This agent intercepts attempts to load class files and outputs package/class info to stdout
+
+# TODO
+ 
+Create an example of preventing classfile caching.
+
+That is to say, implement a means whereby every time a class is instantiated,
+the class definition is loaded from a (Guava) cache which I have defined myself.
--- src/main/java/ie/hunt/poc_agent/FooMain.java
+++ src/main/java/ie/hunt/poc_agent/FooMain.java
@@ -1,10 +1,32 @@
 package ie.hunt.poc_agent;
 
+import java.util.stream.IntStream;
+
 public class FooMain {
 
-    public static void main(String[] args) {
+    public FooMain() {
 
         System.out.println("Hello World");
+    }
 
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        IntStream.range(0, 10).forEach(x ->
+                loadClass()
+        );
+
+    }
+
+
+    private static void loadClass() {
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        new FooMain();
     }
 }
